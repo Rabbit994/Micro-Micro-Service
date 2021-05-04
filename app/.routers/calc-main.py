1e05f93681a896b0bb462d51cd21a40e172a0ef3
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-04 23:48:37 +0000

    Adding exception and changing query parameters

--- app/.routers/calc-main.py
+++ app/.routers/calc-main.py
@@ -1,8 +1,7 @@
 import os
-
 import requests
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -15,20 +14,23 @@ class Calc(BaseModel):
 @router.post("/calc")
 def calc(body: Calc):
     if body.op == "add":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/add?num1={body.number1}&num2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/add?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "subtract":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/subtract?num1={body.number1}&num2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/subtract?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "multiply":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/multiply?num1={body.number1}&num2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/multiply?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "divide":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/divide?num1={body.number1}&num2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/divide?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "expo":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/expo?num1={body.number1}&num2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/expo?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "remainder":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/remainder?num1={body.number1}&num2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/remainder?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
+    else:
+        raise HTTPException(status_code=415, detail={"error": "operation not implemented"})
+
