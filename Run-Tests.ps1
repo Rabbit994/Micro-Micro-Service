6eae5ea69f5c7ec8dc0ac103f21c753309ceb379
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-06 04:03:37 +0000

    Added Tests

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -1,7 +1,9 @@
-Write-Host "Pinging"
+Write-Host "======================== Pinging ========================"
 Invoke-RestMethod -uri 'http://127.0.0.1:9000/math/ping'
 Invoke-RestMethod -uri 'http://127.0.0.1:9000/math/api/ping'
-Write-Host "Doing Math"
+Invoke-RestMethod -uri 'http://127.0.0.1:9000/date/ping'
+Invoke-RestMethod -uri 'http://127.0.0.1:9000/date/api/ping'
+Write-Host "======================== Doing Math ========================"
 Invoke-RestMethod -Uri 'http://127.0.0.1:9000/math/api/add?number1=15&number2=5' -Method GET
 Invoke-RestMethod -Uri 'http://127.0.0.1:9000/math/api/subtract?number1=15&number2=5' -Method GET
 Invoke-RestMethod -Uri 'http://127.0.0.1:9000/math/api/multiply?number1=20&number2=5' -Method GET
@@ -14,4 +16,18 @@ $a = @{
     number2 = 1616981
 }
 Invoke-RestMethod -Uri http://127.0.0.1:9000/math/calc -Method POST -Body ($a | ConvertTo-Json)
+Write-Host "======================== Doing Date ========================"
+Invoke-RestMethod -Uri 'http://127.0.0.1:9000/date/api/today_day'
+Invoke-RestMethod -Uri 'http://127.0.0.1:9000/date/api/today_month'
+Invoke-RestMethod -Uri 'http://127.0.0.1:9000/date/api/today_year'
+Invoke-RestMethod -Uri 'http://127.0.0.1:9000/date/api/today_unix'
+$oper = @('american','iso8601','european')
+foreach($op in $oper){
+    $a = @{
+        format = $op
+    }
+    Invoke-RestMethod -uri 'http://127.0.0.1:9000/date/today' -Method POST -Body ($a | ConvertTo-Json)
+}
+
+
 
