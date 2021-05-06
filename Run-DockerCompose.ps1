db74908cc4bdda5b38f6b7c4fdd7309d5aae5885
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-06 02:10:22 +0000

    adding Nginx and Docker Compose run

---
+++ Run-DockerCompose.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding()]
+param (
+    # Parameter help description
+    [Parameter(Mandatory=$true,HelpMessage="Execution")]
+    [ValidateSet('build','up','stop','down')]
+    [string]
+    $Action
+)
+switch($Action){
+    "build" {
+        Start-Process 'docker-compose' -ArgumentList 'build --no-cache' -NoNewWindow -Wait
+    }
+    "up" {
+        Start-Process 'docker-compose' -ArgumentList '--project-name micro up -d --build' -NoNewWindow -Wait
+        #Start-Process 'docker' -ArgumentList 'system prune -f'
+    }
+    "down" {
+        Start-Process 'docker-compose' -ArgumentList 'down' -NoNewWindow -Wait
+    }
+    "stop" {
+        Start-Process 'docker-compose' -ArgumentList 'stop' -NoNewWindow -Wait
+    }
+}
