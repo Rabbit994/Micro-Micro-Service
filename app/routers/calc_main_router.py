40c36360ee8e6f02ee1373fb0422afc3fb906c24
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-06 01:43:41 +0000

    proper URL

--- app/routers/calc_main_router.py
+++ app/routers/calc_main_router.py
@@ -14,22 +14,22 @@ class Calc(BaseModel):
 @router.post("/")
 def calc(body: Calc):
     if body.op == "add":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/add?number1={body.number1}&number2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/math/api/add?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "subtract":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/subtract?number1={body.number1}&number2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/math/api/subtract?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "multiply":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/multiply?number1={body.number1}&number2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/math/api/multiply?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "divide":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/divide?number1={body.number1}&number2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/math/api/divide?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "expo":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/expo?number1={body.number1}&number2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/math/api/expo?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     elif body.op == "remainder":
-        r = requests.get(url=f"{os.getenv('base_url')}/api/calc/remainder?number1={body.number1}&number2={body.number2}")
+        r = requests.get(url=f"{os.getenv('base_url')}/math/api/remainder?number1={body.number1}&number2={body.number2}")
         return {"result": r.json()['result']}
     else:
         raise HTTPException(status_code=415, detail={"error": "operation not implemented"})
