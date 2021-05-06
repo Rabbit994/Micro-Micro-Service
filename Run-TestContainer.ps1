611b4049ba8718654c7e6332f3dd799c8ed1eca5
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-06 05:20:00 +0000

    Matching Powershell file to docker-compose

--- Run-TestContainer.ps1
+++ Run-TestContainer.ps1
@@ -1,4 +1,4 @@
-Start-Process -FilePath docker -ArgumentList "stop ps-test" -Wait -NoNewWindow
-Start-Process -Filepath docker -ArgumentList "rm ps-test" -Wait -NoNewWindow
-Start-Process -FilePath docker -ArgumentList "build -t ps-test -f PStests/ps_test.dockerfile ." -Wait -NoNewWindow
-Start-Process -Filepath docker -ArgumentList "run -i --network=""micro_frontend"" --name ps-test ps-test" -Wait -NoNewWindow
+Start-Process -FilePath docker -ArgumentList "stop ps-tests" -Wait -NoNewWindow
+Start-Process -Filepath docker -ArgumentList "rm ps-tests" -Wait -NoNewWindow
+Start-Process -FilePath docker -ArgumentList "build -t ps-tests -f PStests/ps_test.dockerfile ." -Wait -NoNewWindow
+Start-Process -Filepath docker -ArgumentList "run -i --network=""micro_frontend"" --name ps-tests ps-tests" -Wait -NoNewWindow
