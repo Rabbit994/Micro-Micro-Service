bc5ac5a86366a13b22533ebcfba5c69df7c9695a
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-06 05:10:42 +0000

    Adding project name to all docker compose commands

--- Run-DockerCompose.ps1
+++ Run-DockerCompose.ps1
@@ -8,16 +8,16 @@ param (
 )
 switch($Action){
     "build" {
-        Start-Process 'docker-compose' -ArgumentList 'build --no-cache' -NoNewWindow -Wait
+        Start-Process 'docker-compose' -ArgumentList '--project-name micro build --no-cache' -NoNewWindow -Wait
     }
     "up" {
         Start-Process 'docker-compose' -ArgumentList '--project-name micro up -d --build' -NoNewWindow -Wait
         #Start-Process 'docker' -ArgumentList 'system prune -f'
     }
     "down" {
-        Start-Process 'docker-compose' -ArgumentList 'down' -NoNewWindow -Wait
+        Start-Process 'docker-compose' -ArgumentList '--project-name micro down' -NoNewWindow -Wait
     }
     "stop" {
-        Start-Process 'docker-compose' -ArgumentList 'stop' -NoNewWindow -Wait
+        Start-Process 'docker-compose' -ArgumentList '--project-name micro stop' -NoNewWindow -Wait
     }
 }
