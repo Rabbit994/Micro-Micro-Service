d78c12d5927180281273e79072f33e68d6bd4af1
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-13 18:58:05 +0000

    name_micro initialization

--- app/name_micro.py
+++ app/name_micro.py
@@ -3,7 +3,7 @@ import os
 from fastapi import APIRouter, FastAPI
 from pydantic import main
 
-from .routers import date_micro_router
+from .routers import name_micro_router
 
 app = FastAPI()
 
@@ -12,7 +12,7 @@ def ping():
     return 'pong'
 
 app.include_router(
-    date_micro_router.router,
-    tags=["date_micro"],
+    name_micro_router.router,
+    tags=["name_micro"],
     responses={404: {"description": "Not found"}}
 )
