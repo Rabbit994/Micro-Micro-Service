9b780c5947be6fba5c68cddbc428ff1a25f015e5
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-14 18:25:57 +0000

    name main router for add/get/delete

--- app/routers/name_main_router.py
+++ app/routers/name_main_router.py
@@ -6,21 +6,50 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
-class User:
-    user_id: str
-
 @router.get("/user")
-def user(userid:str):
-    pass
+def user_get(userid:str):
+    sess = requests.Session()
+    r = sess.get(url=f"{os.getenv('base_url')}/name/api/first?userid={userid}")
+    first_name = r.json()['first_name']
+    r = sess.get(url=f"{os.getenv('base_url')}/name/api/surname?userid={userid}")
+    surname = r.json()['surname']
+    r = sess.get(url=f"{os.getenv('base_url')}/name/api/email?userid={userid}")
+    email = r.json()['email']
+    return {
+        "userid": userid,
+        "first_name": first_name,
+        "surname": surname,
+        "email": email
+    }
 
 @router.post("/user")
 def user_new():
-    pass
+    sess = requests.Session()
+    r = sess.post(url=f"{os.getenv('base_url')}/name/api/userid")
+    userid =  r.json()['userid']
+    r = sess.post(url=f"{os.getenv('base_url')}/name/api/first?userid={userid}")
+    first_name = r.json()['first_name']
+    r = sess.post(url=f"{os.getenv('base_url')}/name/api/surname?userid={userid}")
+    surname = r.json()['surname']
+    r = sess.post(url=f"{os.getenv('base_url')}/name/api/email?userid={userid}")
+    email = r.json()['email']
+    return {
+        "userid": userid,
+        "first_name": first_name,
+        "surname": surname,
+        "email": email
+    }
 
 @router.delete("/user")
 def user_delete(userid:str):
-    pass
-
+    sess = requests.Session()
+    r = sess.delete(url=f"{os.getenv('base_url')}/name/api/userid")
+    r = sess.delete(url=f"{os.getenv('base_url')}/name/api/first?userid={userid}")
+    r = sess.delete(url=f"{os.getenv('base_url')}/name/api/surname?userid={userid}")
+    r = sess.delete(url=f"{os.getenv('base_url')}/name/api/email?userid={userid}")
+    return {
+        "userid": userid
+    }
 
 
 
