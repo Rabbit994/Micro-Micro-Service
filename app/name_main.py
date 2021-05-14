20488215d53d150e8825668ccc38db4bcc23b538
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-05-14 18:26:31 +0000

    creating name-main file

--- app/name_main.py
+++ app/name_main.py
@@ -3,7 +3,7 @@ import os
 from fastapi import APIRouter, FastAPI
 from pydantic import main
 
-from .routers import date_main_router
+from .routers import name_main_router
 
 app = FastAPI()
 
@@ -12,7 +12,7 @@ def ping():
     return 'pong'
 
 app.include_router(
-    date_main_router.router,
-    tags=["date"],
+    name_main_router.router,
+    tags=["name"],
     responses={404: {"description": "Not found"}}
-)    
+)
