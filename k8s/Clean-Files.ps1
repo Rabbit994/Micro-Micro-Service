f377fb3a14311bf743e5f5bd003d0406dd52483e
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-07-14 22:32:42 -0400

    Configuring to ignore kubeconfig file

--- k8s/Clean-Files.ps1
+++ k8s/Clean-Files.ps1
@@ -1,1 +1,1 @@
-Get-ChildItem -Recurse -include *.yaml | where {$_.Name -notlike "*-base.yaml"} | Remove-Item -Force:$true -Confirm:$false 
+Get-ChildItem -Recurse -include *.yaml -Exclude kubeconfig.yaml | where {$_.Name -notlike "*-base.yaml"} | Remove-Item -Force:$true -Confirm:$false
